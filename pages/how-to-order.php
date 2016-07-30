d31f88b42e1d40cc54c93696b7f31df35da08ffc
granaderos <marejeanperpinosa@gmail.com>
2016-07-30 12:13:10 +0800

    first real commit is the directory structure. :)

---
+++ pages/how-to-order.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>How to Order</title>
+</head>
+<body>
+    <?php include_once "header.html";?>
+
+
+    <?php include_once "footer.html";?>
+</body>
+</html>
---
+++ pages/products.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Products</title>
+</head>
+<body>
+    <?php include_once "header.html";?>
+
+
+    <?php include_once "footer.html";?>
+</body>
+</html>
